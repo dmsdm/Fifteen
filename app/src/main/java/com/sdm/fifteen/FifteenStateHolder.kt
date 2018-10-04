84f9b065ea8709f7eaf086ba7f2e6ce0f1181375
d.smirnov <d.smirnov@s-e.su>
2018-10-04 14:12:33 +0300

    removed excess shuffle

--- app/src/main/java/com/sdm/fifteen/FifteenStateHolder.kt
+++ app/src/main/java/com/sdm/fifteen/FifteenStateHolder.kt
@@ -4,7 +4,7 @@ import java.util.*
 
 object FifteenStateHolder {
 
-    var items = getSolvableList(MutableList(16) { i -> if (i < 15) (i + 1) else 0 }.shuffled())
+    var items = getSolvableList(MutableList(16) { i -> if (i < 15) (i + 1) else 0 })
 
     private fun getSolvableList(items: List<Int>): List<Int> {
         var newItems = items.shuffled()
